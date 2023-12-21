7ec23fd91c1b052c11e231e11dcf1e98514e8a3f
94v1c8 <8792065+94v1c8@users.noreply.github.com>
2023-12-21 19:54:06 +1030

    commit

--- linux/etc/profile.d/zzz-94v1c8.sh
+++ linux/etc/profile.d/zzz-94v1c8.sh
@@ -1,6 +1,47 @@
-#!/bin/sh
 # shellcheck shell=sh
+# shellcheck source=/dev/null
 
-# Custom user configuration directory for compliant XDG packages.
-XDG_CONFIG_HOME="$HOME/.config"
-export XDG_CONFIG_HOME
+# Do not continue if run directly, rather than being sourced.
+# if [ ! "$0" != "$_" ]; then
+#     return 0
+# fi
+
+# Function to run a specified command (optionally with parameters), then test
+#   if completed successfully, if not true then break out of script.
+function _commandok() {
+    if ! $* 2>/dev/null; then return 0; fi
+}
+
+# Function to unset an existing environment variable, and then set it anew
+# with the specified value.
+function _envclobber() {
+    xENVARR=($*)
+    for xENV in "${xENVARR[@]}" ; do
+        _commandok "unset -v ${xENV%%:*}"
+        _commandok "mkdir --parents "${xENV#*:}""
+        export "${xENV%%:*}"="${xENV#*:}"
+    done
+}
+
+# Function to prepend a unique value to an existing environment variable.
+function _envprepend() {
+    xENVARR=($*)
+    for xENV in "${xENVARR[@]}" ; do
+        _commandok "mkdir --parents "${xENV#*:}""
+        
+    done
+}
+
+# Create symlinks with hirearchy for files that have been created by vendor
+#   as means of standardising configuration across like instances.
+function copy_zzz() {
+    updatedb &>/dev/null
+    local FILES=(
+        $(cat "${HOME}/.custom/.addl_files")
+        $(locate -b zzz-94v1c8 | grep -Ev "^${HOME}")
+    )
+    for FILE in ${FILES[*]}; do
+        mkdir -p "${HOME}/.custom/$(dirname ${FILE})" &>/dev/null
+        ln -sv "${FILE}" "${HOME}/.custom${FILE}" 2>/dev/null
+    done
+}
---
+++ linux/home/.config/.bashrc
@@ -0,0 +1,7 @@
+# shellcheck shell=bash
+# shellcheck source=/dev/null
+
+# Do not continue if run directly, rather than being sourced.
+# if [ ! "$0" != "$_" ]; then
+#     return 0
+# fi
